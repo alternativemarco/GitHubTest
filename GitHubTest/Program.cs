65537aa75a7bedfc24c00074e6e7bbc9f4e253f0
infba08 <infba08@vereb.dc>
2019-09-20 16:39:22 +0200

    Add project files.

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hogy hívnak? ");
+            String nev = Console.ReadLine();
+            Console.WriteLine("Oh, milyen szép név a " + nev + "!");
+            Console.WriteLine("Szerecc programozni?");
+            String v = Console.ReadLine();
+            if(v.ToLower() == "igen")
+            {
+                Console.WriteLine("Jaj de jó, mi nagyon jó barátok leszünk!");
+            }
+            else
+            {
+                Console.WriteLine("Hát akko kabe!");
+            }
+
+            //TODO: csijjag
+            Console.ReadKey();
+        }
+    }
+}
